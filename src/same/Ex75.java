4ade054d5238ea0ae0062a8701ea2bf10ea93821
Apple <apple@192-168-1-110.tpgi.com.au>
2022-04-01 10:39:57 +1100

    My third program

---
+++ src/same/Ex75.java
@@ -0,0 +1,10 @@
+package same;
+
+public class Ex75 {
+    public boolean ele(int[] array){
+        if(array[0] ==(array [array.length-1])){
+            return true;
+        }
+        return false;
+    }
+}
